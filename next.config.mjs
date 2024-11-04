bdb3c1d46a4f5410b51a09fa04a642d53d87699d
alice <aliceqininus@gmail.com>
2024-11-04 13:50:12 +0800

    update images host config in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: ['firebasestorage.googleapis.com'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/diet-exercise-tracker.appspot.com/o/**",
       },
+    ],
+  },
 };
 
 export default nextConfig;
