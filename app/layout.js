6fc2620bc17c28a705208daf719fe2fe32fd0f3f
alice <aliceqininus@gmail.com>
2024-11-05 15:31:59 +0800

    add Microsoft Clarity tracking code

--- app/layout.js
+++ app/layout.js
@@ -36,6 +36,14 @@ export default function RootLayout({ children }) {
           gtag('js', new Date());
           gtag('config', 'G-LXCC1J48MT');`}
         </script>
+        {/* Microsoft Clarity */}
+        <script type="text/javascript">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "otozvyq0wi");`}
+        </script>
       </head>
       <body
         className={`w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col ${opensans.className} bg-purple-50 dark:bg-sky-50 text-stone-700`}
